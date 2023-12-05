727afdc9f7043c59799bb32b5cc031dff6688f34
kyleranous <kranous05@gmail.com>
2023-12-04 22:36:02 -0700

    Added order_injection properties for dealing with the order_injection handler

--- newstore_connector/ns_connector.py
+++ newstore_connector/ns_connector.py
@@ -14,17 +14,21 @@ class NewStoreConnector(APIConnector):
         """
         Initialize the NewStoreConnector Class
         """
+        # Populate parameters and establish the connection
         self._validate_init_params(**kwargs)
         # Initialize the APIConnector Class features
         super().__init__(**kwargs)
         # Set the tenant information for the NewStore API
         self.tenant = kwargs.get("tenant")
-        self.env = kwargs.get("enc", "p")
+        self.env = kwargs.get("env", "p")
         self._base_url = f"https://{self.tenant}.{self.env}.newstore.net/"
         self.client_id = kwargs.get("client_id")
         self.client_secret = kwargs.get("client_secret")
         self.role = kwargs.get("role", "iam:providers:read")
-        self.token = kwargs.get("token", self._get_auth_token())
+        self.token = kwargs.get("token") or self._get_auth_token()
+
+        # Initialize empty instance variables for the API Classes
+        self._order_injection = None
 
     def _validate_init_params(self, **kwargs):
         """
@@ -70,9 +74,45 @@ class NewStoreConnector(APIConnector):
         response.raise_for_status()
         return response.json().get("access_token")
 
+    def _get_headers(self):
+        """
+        Return the basic header for making NewStore API Requests
+        """
+        return {
+            'Authorization': f'Bearer {self.token}'
+        }
+
     @property
     def base_url(self):
         """
         Return the base URL for the NewStore API
         """
         return self._base_url
+
+    # Define the API Classes as Properties Here
+    # This allows us to only initialize the API Classes when they are needed
+    # pylint: disable=import-outside-toplevel
+    @property
+    def order_injection(self):
+        """
+        Return the appropriate OrderInjection Class for the NewStore API
+        """
+        if not self._order_injection:
+            self.order_injection = "0.1"
+
+        return self._order_injection
+
+    @order_injection.setter
+    def order_injection(self, value):
+        """
+        Set the OrderInjection Class based on the version passed to the setter
+        """
+        if value == "0.1":
+            from .order_injection import OrderInjectionV01
+
+            self._order_injection = OrderInjectionV01()
+            self._order_injection.headers = self._get_headers()
+            self._order_injection.session = self.session
+            self._order_injection.base_url = self.base_url
+        else:
+            raise ValueError(f"Invalid OrderInjection Version: {value}")
