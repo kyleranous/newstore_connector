3c7554b670a144734b1933e180cbfefdc8ba8c52
Kyle Ranous <kranous05@gmail.com>
2023-12-13 16:36:54 -0700

    Maybe this time?

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "https://github.com/kyleranous/api_toolkit.git",
+        'git+https://github.com/kyleranous/api_toolkit.git@main#egg=api_toolkit',
         'requests'
     ]
 )
