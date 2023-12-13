676fedb9c48d00d30a912cd8496845c0cb136627
Kyle Ranous <kranous05@gmail.com>
2023-12-13 16:40:03 -0700

    Maybe this works?

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/kyleranous/api_toolkit.git@main#egg=api_toolkit',
+        'api_toolkit',
         'requests'
+    ],
+    dependency_links=[
+        'git+https://github.com/kyleranous/api_toolkit.git@main#egg=api_toolkit'
     ]
 )
